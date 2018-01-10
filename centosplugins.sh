78ef93345679dae729f00eed4afde6008bd6bc52
Northeaster <35185688+vivoxy@users.noreply.github.com>
2018-01-10 05:05:10 +0100

    Update centosplugins.sh

--- centosplugins.sh
+++ centosplugins.sh
@@ -7,8 +7,8 @@ echo "# (0) Bash Scripti Güncelle                                    #"
 echo "# (1) Centos Installing "SSH2" Extension                           	     #"
 echo "# (2) Centos Installing "MONO" Extension                              #"
 echo "# (3) Centos Installing "Mod Pagespeed" Extension                  #"
-echo "# (4) Litespeed Servisini Yeniden Başlat                        	   #"
-echo "# (5) DNS Sunucusunu Yeniden Başlat                             	   #"
+echo "# (4) Centos Installing "Dstat" Extension                         	   #"
+echo "# (5) Centos Installing "MongoDB" Extension                            	   #"
 echo "# (6) Posta Sunucusunu Yeniden Başlat                              	   #"
 echo "# (7) SSH  Sunucusunu Yeniden Başlat                               	   #"
 echo "# (8) İmap  Sunucusunu Yeniden Başlat                              	   #"
@@ -47,9 +47,13 @@ clear
 echo "SSH 2 Kuruluyor..."
 sleep 3
 yum -y install gcc php-devel php-pear make libssh2 libssh2-devel
+sleep 1
 pecl install -f ssh2
+sleep 1
 echo extension=ssh2.so >> /usr/local/lib/php.ini
+sleep 1
 service httpd restart && service nginx restart && service lsws restart
+sleep 1
 echo "SSH 2 Eklentisi Kurulmuştur..."
 ;;
 2)
@@ -57,9 +61,13 @@ clear
 echo "Mono Kuruluyor..."
 sleep 3
 yum install yum-utils
+sleep 1
 rpm --import "http://keyserver.ubuntu.com/pks/lookup?op=get&search=0x3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF"
+sleep 1
 yum-config-manager --add-repo http://download.mono-project.com/repo/centos6/
+sleep 1
 yum install mono-devel
+sleep 1
 echo "MONO Eklentisi Kurulmuştur..."
 ;;
 3)
@@ -67,31 +75,62 @@ clear
 echo "Mod Pagespeed Kuruluyor..."
 sleep 3
 echo extension=[mod-pagespeed] >> /etc/yum.repos.d/mod-pagespeed.repo
+sleep 1
 echo extension=name=mod-pagespeed >> /etc/yum.repos.d/mod-pagespeed.repo
+sleep 1
 echo extension=baseurl=http://dl.google.com/linux/mod-pagespeed/rpm/stable/x86_64 >> /etc/yum.repos.d/mod-pagespeed.repo
+sleep 1
 echo extension=enabled=1 >> /etc/yum.repos.d/mod-pagespeed.repo
+sleep 1
 echo extension=gpgcheck=0 >> /etc/yum.repos.d/mod-pagespeed.repo
+sleep 1
 yum repolist
+sleep 1
 yum install mod-pagespeed -y
+sleep 1
 echo extension=ModPagespeed on >> /etc/httpd/conf.d/pagespeed.conf
+sleep 1
 echo "Reboot Atılıyor..."
 sleep
 reboot
 ;;
 4)
 clear
-echo "Litespeed Servisi Yeniden Başlatılıyor, lütfen bekleyiniz..."
+echo "Dstat Kuruluyor..."
 sleep 3
-service lsws restart
-echo "Litespeed Servisi Yeniden Başlatıldı."
+yum install update -y
+sleep 1
+yum -y install dstat
+sleep 1
+echo "Dstat Eklentisi Kurulmuştur... Eklentisi açmak için ssh ekranında --dstat-- yazınız"
 ;;
 5)
 clear
-echo "DNS Sunucusu Yeniden Başlatılıyor, lütfen bekleyiniz..."
+echo "MongoDB Kuruluyor..."
 sleep 3
-service named restart
-echo "DNS Sunucusu Yeniden Başlatıldı."
-;;
+yum install update -y
+sleep 1
+yum -y install nano
+sleep 1
+echo extension=[mongodb] >> /etc/yum.repos.d/mongodb.repo
+sleep 1
+echo extension=name=MongoDB Repository >> /etc/yum.repos.d/mongodb.repo
+sleep 1
+echo extension=baseurl=http://downloads-distro.mongodb.org/repo/redhat/os/x86_64/ >> /etc/yum.repos.d/mongodb.repo
+sleep 1
+echo extension=gpgcheck=0 >> /etc/yum.repos.d/mongodb.repo
+sleep 1
+echo extension=enabled=1 >> /etc/yum.repos.d/mongodb.repo
+sleep 1
+yum -y install mongo-10gen mongo-10gen-server
+sleep 1
+chkconfig mongod on
+sleep 1
+service mongod start
+sleep 1
+service mongod status
+sleep 1
+echo "MongoDB Eklentisi Kurulmuştur... "
 6)
 clear
 echo "Posta Sunucusu Yeniden Başlatılıyor, lütfen bekleyiniz..."
